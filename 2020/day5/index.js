5db29ff165615eaef4b77579d0089090c8629321
carlssonemil <hello@emilcarlsson.se>
2020-12-06 16:01:29 +0100

    Move range method to utils folder

--- 2020/day5/index.js
+++ 2020/day5/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const range = require('./range.js');
+const range = require('../../utils/range.js');
 const missingNum = require('./missingNum.js');
 
 let data = fs.readFileSync('input.txt', 'utf-8');
